24d86968d59272c7bf9b0cca05abc9165be9f028
ivandeoliveira-py <ivandeoliveira.psd@gmail.com>
2026-05-05 15:44:38 -0300

    chore(devops): configure dockerized development environment

---
+++ src/main/java/org/compilaceone/complianceone/ComplianceOneApplication.java
@@ -0,0 +1,13 @@
+package org.compilaceone.complianceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplianceOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/compilaceone/complianceone/ComplianceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.compilaceone.complianceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplianceOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
